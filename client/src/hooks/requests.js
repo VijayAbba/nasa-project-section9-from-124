bd273037715a3cba760cad02a3b80277f4b6b806
VijayAbba <vijaykrishna.ed@gmail.com>
2023-10-17 00:10:38 +0530

    class 126 Completed POST /launches

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,13 +1,11 @@
 const API_URL = "http://localhost:8000";
 
-// TODO: Once API is ready.
 // Load planets and return as JSON.
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
   return await response.json();
 }
 
-// TODO: Once API is ready.
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`);
@@ -15,14 +13,22 @@ async function httpGetLaunches() {
   return fetchedLaunches.sort((a, b) => a.flightNumber - b.flightNumber);
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (err) {
+    return { ok: false };
+  }
 }
 
-async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
-}
+// Delete launch with given ID.
+async function httpAbortLaunch(id) {}
 
 export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
