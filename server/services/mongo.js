564b75984bda8ea448e3735ba373d285efcc98dd
VijayAbba <vijaykrishna.ed@gmail.com>
2023-10-23 23:29:08 +0530

    202. Managing Secrets With Dotenv

--- server/services/mongo.js
+++ server/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:5z0n6UHAWf2UobMG@clusternasa.k9s4nsw.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URLs;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
